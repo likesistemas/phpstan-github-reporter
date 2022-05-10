6ce4f7a8956100829b2a0efb7489311ef867e357
Ricardo Paes <ricardo@likesistemas.com.br>
2022-05-10 10:57:54 -0300

    💚 Removing PHP 5.6

--- src/GithubReporter.php
+++ src/GithubReporter.php
@@ -2,13 +2,73 @@
 
 namespace Like\Phpstan;
 
-use PHPStan\Command\ErrorFormatter\GithubErrorFormatter;
 use PHPStan\Command\AnalysisResult;
 use PHPStan\Command\Output;
+use PHPStan\File\RelativePathHelper;
+use PHPStan\Command\ErrorFormatter\ErrorFormatter;
+use function array_walk;
+use function implode;
+use function sprintf;
+use function str_replace;
 
-class GithubReporter extends GithubErrorFormatter {
+/**
+ * Allow errors to be reported in pull-requests diff when run in a GitHub Action
+ *
+ * @see https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
+ */
+class GithubReporter implements ErrorFormatter {
+	public function __construct(
+		private RelativePathHelper $relativePathHelper,
+		private ErrorFormatter $errorFormatter,
+	) {
+	}
+
 	public function formatErrors(AnalysisResult $analysisResult, Output $output): int {
-		$return = parent::formatErrors($analysisResult, $output);
-		return $return;
+		$this->errorFormatter->formatErrors($analysisResult, $output);
+
+		foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
+			$metas = [
+				'file' => $this->relativePathHelper->getRelativePath($fileSpecificError->getFile()),
+				'line' => $fileSpecificError->getLine(),
+				'col' => 0,
+			];
+			array_walk($metas, static function (&$value, string $key): void {
+				$value = sprintf('%s=%s', $key, (string) $value);
+			});
+
+			$message = $fileSpecificError->getMessage();
+			// newlines need to be encoded
+			// see https://github.com/actions/starter-workflows/issues/68#issuecomment-581479448
+			$message = str_replace("\n", '%0A', $message);
+
+			$line = sprintf('::error %s::%s', implode(',', $metas), $message);
+
+			$output->writeRaw($line);
+			$output->writeLineFormatted('');
+		}
+
+		foreach ($analysisResult->getNotFileSpecificErrors() as $notFileSpecificError) {
+			// newlines need to be encoded
+			// see https://github.com/actions/starter-workflows/issues/68#issuecomment-581479448
+			$notFileSpecificError = str_replace("\n", '%0A', $notFileSpecificError);
+
+			$line = sprintf('::error ::%s', $notFileSpecificError);
+
+			$output->writeRaw($line);
+			$output->writeLineFormatted('');
+		}
+
+		foreach ($analysisResult->getWarnings() as $warning) {
+			// newlines need to be encoded
+			// see https://github.com/actions/starter-workflows/issues/68#issuecomment-581479448
+			$warning = str_replace("\n", '%0A', $warning);
+
+			$line = sprintf('::warning ::%s', $warning);
+
+			$output->writeRaw($line);
+			$output->writeLineFormatted('');
+		}
+
+		return $analysisResult->hasErrors() ? 1 : 0;
 	}
 }
